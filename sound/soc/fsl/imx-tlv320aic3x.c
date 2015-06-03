387a093ed10e3560423b55e22c27787f7700600d
orokach <orokach@users.noreply.github.com>
2015-06-03 12:36:31 +0300

    Update imx-tlv320aic3x.c

--- sound/soc/fsl/imx-tlv320aic3x.c
+++ sound/soc/fsl/imx-tlv320aic3x.c
@@ -17,381 +17,55 @@
  */
 
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/of_platform.h>
-#include <linux/of_i2c.h>
-#include <linux/of_gpio.h>
-#include <linux/slab.h>
-#include <linux/gpio.h>
+#include <linux/i2c.h>
 #include <linux/clk.h>
 #include <sound/soc.h>
-#include <sound/jack.h>
-#include <sound/pcm_params.h>
-#include <sound/soc-dapm.h>
-#include <linux/pinctrl/consumer.h>
 
-#include "../codecs/wm8962.h"
 #include "../codecs/tlv320aic3x.h"
 #include "imx-audmux.h"
 
 #define DAI_NAME_SIZE	32
 
-struct imx_tlv320aic3x_data {
+struct imx_sgtl5000_data {
 	struct snd_soc_dai_link dai;
 	struct snd_soc_card card;
 	char codec_dai_name[DAI_NAME_SIZE];
 	char platform_name[DAI_NAME_SIZE];
+	struct clk *codec_clk;
 	unsigned int clk_frequency;
 };
-
-struct imx_priv {
-	int hp_gpio;
-	int hp_active_low;
-	int mic_gpio;
-	int mic_active_low;
-	bool amic_mono;
-	bool dmic_mono;
-	struct snd_soc_codec *codec;
-	struct platform_device *pdev;
-	struct snd_pcm_substream *first_stream;
-	struct snd_pcm_substream *second_stream;
-};
-static struct imx_priv card_priv;
-
-static struct snd_soc_jack imx_hp_jack;
-static struct snd_soc_jack_pin imx_hp_jack_pins[] = {
-	{
-		.pin = "Headphone Jack",
-		.mask = SND_JACK_HEADPHONE,
-	},
-};
-static struct snd_soc_jack_gpio imx_hp_jack_gpio = {
-	.name = "headphone detect",
-	.report = SND_JACK_HEADPHONE,
-	.debounce_time = 250,
-	.invert = 0,
-};
-
-static struct snd_soc_jack imx_mic_jack;
-static struct snd_soc_jack_pin imx_mic_jack_pins[] = {
-	{
-		.pin = "AMIC",
-		.mask = SND_JACK_MICROPHONE,
-	},
-};
-static struct snd_soc_jack_gpio imx_mic_jack_gpio = {
-	.name = "microphone detect",
-	.report = SND_JACK_MICROPHONE,
-	.debounce_time = 250,
-	.invert = 0,
-};
-
-static int hpjack_status_check(void)
+static int imx_sgtl5000_dai_init(struct snd_soc_pcm_runtime *rtd)
 {
-	struct imx_priv *priv = &card_priv;
-	struct platform_device *pdev = priv->pdev;
-	char *envp[3], *buf;
-	int hp_status, ret;
+	struct imx_sgtl5000_data *data = container_of(rtd->card,
+					struct imx_sgtl5000_data, card);
+	struct device *dev = rtd->card->dev;
+	int ret;
 
-	if (!gpio_is_valid(priv->hp_gpio))
-		return 0;
-
-	hp_status = gpio_get_value(priv->hp_gpio) ? 1 : 0;
-
-	buf = kmalloc(32, GFP_ATOMIC);
-	if (!buf) {
-		dev_err(&pdev->dev, "%s kmalloc failed\n", __func__);
-		return -ENOMEM;
+	ret = snd_soc_dai_set_sysclk(rtd->codec_dai, 0,
+				     data->clk_frequency, SND_SOC_CLOCK_IN);
+	if (ret) {
+		dev_err(dev, "could not set codec driver clock params\n");
+		return ret;
 	}
 
-	if (hp_status != priv->hp_active_low) {
-		snprintf(buf, 32, "STATE=%d", 2);
-		snd_soc_dapm_disable_pin(&priv->codec->dapm, "Ext Spk");
-		ret = imx_hp_jack_gpio.report;
-	} else {
-		snprintf(buf, 32, "STATE=%d", 0);
-		snd_soc_dapm_enable_pin(&priv->codec->dapm, "Ext Spk");
-		ret = 0;
-	}
-
-	envp[0] = "NAME=headphone";
-	envp[1] = buf;
-	envp[2] = NULL;
-	kobject_uevent_env(&pdev->dev.kobj, KOBJ_CHANGE, envp);
-	kfree(buf);
-
-	return ret;
+	return 0;
 }
 
-static int micjack_status_check(void)
-{
-	struct imx_priv *priv = &card_priv;
-	struct platform_device *pdev = priv->pdev;
-	char *envp[3], *buf;
-	int mic_status, ret;
-
-	if (!gpio_is_valid(priv->mic_gpio))
-		return 0;
-
-	mic_status = gpio_get_value(priv->mic_gpio) ? 1 : 0;
-
-	if ((mic_status != priv->mic_active_low && priv->amic_mono)
-		|| (mic_status == priv->mic_active_low && priv->dmic_mono))
-		snd_soc_update_bits(priv->codec, WM8962_THREED1,
-				WM8962_ADC_MONOMIX_MASK, WM8962_ADC_MONOMIX);
-	else
-		snd_soc_update_bits(priv->codec, WM8962_THREED1,
-				WM8962_ADC_MONOMIX_MASK, 0);
-
-	buf = kmalloc(32, GFP_ATOMIC);
-	if (!buf) {
-		dev_err(&pdev->dev, "%s kmalloc failed\n", __func__);
-		return -ENOMEM;
-	}
-
-	if (mic_status != priv->mic_active_low) {
-		snprintf(buf, 32, "STATE=%d", 2);
-		snd_soc_dapm_disable_pin(&priv->codec->dapm, "DMIC");
-		ret = imx_mic_jack_gpio.report;
-	} else {
-		snprintf(buf, 32, "STATE=%d", 0);
-		snd_soc_dapm_enable_pin(&priv->codec->dapm, "DMIC");
-		ret = 0;
-	}
-
-	envp[0] = "NAME=microphone";
-	envp[1] = buf;
-	envp[2] = NULL;
-	kobject_uevent_env(&pdev->dev.kobj, KOBJ_CHANGE, envp);
-	kfree(buf);
-
-	return ret;
-}
-
-
-static const struct snd_soc_dapm_widget imx_tlv320aic3x_dapm_widgets[] = {
-	SND_SOC_DAPM_HP("Headphone Jack", NULL),
-	SND_SOC_DAPM_LINE("Line Out", NULL),
+static const struct snd_soc_dapm_widget imx_sgtl5000_dapm_widgets[] = {
 	SND_SOC_DAPM_MIC("Mic Jack", NULL),
-	SND_SOC_DAPM_LINE("Line In", NULL),
+	SND_SOC_DAPM_LINE("Line In Jack", NULL),
+	SND_SOC_DAPM_HP("Headphone Jack", NULL),
+	SND_SOC_DAPM_SPK("Line Out Jack", NULL),
+	SND_SOC_DAPM_SPK("Ext Spk", NULL),
 };
 
-static int imx_hifi_hw_params(struct snd_pcm_substream *substream,
-				     struct snd_pcm_hw_params *params)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct imx_priv *priv = &card_priv;
-	struct device *dev = &priv->pdev->dev;
-	struct snd_soc_card *card = codec_dai->codec->card;
-	struct imx_tlv320aic3x_data *data = snd_soc_card_get_drvdata(card);
-	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
-	u32 dai_format;
-	int ret = 0;
-
-
-	dai_format = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBM_CFM;
-	/* set codec DAI configuration */
-	ret = snd_soc_dai_set_fmt(codec_dai, dai_format);
-	if (ret) {
-		dev_err(dev, "failed to set codec dai fmt: %d\n", ret);
-		return ret;
-	}
-
-	/* set cpu DAI configuration */
-	ret = snd_soc_dai_set_fmt(cpu_dai, dai_format);
-	if (ret) {
-		dev_err(dev, "failed to set codec dai fmt: %d\n", ret);
-		return ret;
-	}
-
-	/* set i.MX active slot mask */
-	ret = snd_soc_dai_set_tdm_slot(cpu_dai, 0xfffffffc, 0xfffffffc, 2, 32);
-	if (ret) {
-		dev_err(dev, "failed to set TDM: %d\n", ret);
-		return ret;
-	}
-
-	/* set the codec system clock */
-	ret = snd_soc_dai_set_sysclk(codec_dai, 0, data->clk_frequency, SND_SOC_CLOCK_OUT);
-	if (ret) {
-		dev_err(dev, "failed to set codec SYSCLK: %d\n", ret);
-		return ret;
-	}
-
-	/* set the CPU system clock */
-	ret = snd_soc_dai_set_sysclk(cpu_dai, 0, data->clk_frequency, SND_SOC_CLOCK_OUT);
-	if (ret) {
-		dev_err(dev, "failed to set cpu SYSCLK: %d\n", ret);
-		return ret;
-	}
-
-	return 0;
-}
-
-static int imx_hifi_hw_free(struct snd_pcm_substream *substream)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	struct imx_priv *priv = &card_priv;
-	struct device *dev = &priv->pdev->dev;
-	int ret;
-
-	/* We don't need to handle anything if there's no substream running */
-	if (!priv->first_stream)
-		return 0;
-
-	if (priv->first_stream == substream)
-		priv->first_stream = priv->second_stream;
-	priv->second_stream = NULL;
-
-	if (!priv->first_stream) {
-		/*
-		 * Continuously setting FLL would cause playback distortion.
-		 * We can fix it just by mute codec after playback.
-		 */
-		if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
-			snd_soc_dai_digital_mute(codec_dai, 1, substream->stream);
-
-		/*
-		 * WM8962 doesn't allow us to continuously setting FLL,
-		 * So we set MCLK as sysclk once, which'd remove the limitation.
-		 */
-		ret = snd_soc_dai_set_sysclk(codec_dai, CLKIN_MCLK,
-				0, SND_SOC_CLOCK_IN);
-		if (ret < 0) {
-			dev_err(dev, "failed to switch away from FLL: %d\n", ret);
-			return ret;
-		}
-
-		/* Disable FLL and let codec do pm_runtime_put() */
-		ret = snd_soc_dai_set_pll(codec_dai, WM8962_FLL,
-				WM8962_FLL_MCLK, 0, 0);
-		if (ret < 0) {
-			dev_err(dev, "failed to stop FLL: %d\n", ret);
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
-static struct snd_soc_ops imx_hifi_ops = {
-	.hw_params = imx_hifi_hw_params,
-	.hw_free = imx_hifi_hw_free,
-};
-
-static int imx_tlv320aic3x_gpio_init(struct snd_soc_pcm_runtime *rtd)
-{
-	struct snd_soc_codec *codec = rtd->codec;
-	struct imx_priv *priv = &card_priv;
-
-	priv->codec = codec;
-
-	if (gpio_is_valid(priv->hp_gpio)) {
-		imx_hp_jack_gpio.gpio = priv->hp_gpio;
-		imx_hp_jack_gpio.jack_status_check = hpjack_status_check;
-
-		snd_soc_jack_new(codec, "Headphone Jack", SND_JACK_HEADPHONE, &imx_hp_jack);
-		snd_soc_jack_add_pins(&imx_hp_jack,
-				ARRAY_SIZE(imx_hp_jack_pins), imx_hp_jack_pins);
-		snd_soc_jack_add_gpios(&imx_hp_jack, 1, &imx_hp_jack_gpio);
-	}
-
-	if (gpio_is_valid(priv->mic_gpio)) {
-		imx_mic_jack_gpio.gpio = priv->mic_gpio;
-		imx_mic_jack_gpio.jack_status_check = micjack_status_check;
-
-		snd_soc_jack_new(codec, "AMIC", SND_JACK_MICROPHONE, &imx_mic_jack);
-		snd_soc_jack_add_pins(&imx_mic_jack,
-				ARRAY_SIZE(imx_mic_jack_pins), imx_mic_jack_pins);
-		snd_soc_jack_add_gpios(&imx_mic_jack, 1, &imx_mic_jack_gpio);
-	} else if (priv->amic_mono || priv->dmic_mono) {
-		/*
-		 * Permanent set monomix bit if only one microphone
-		 * is present on the board while it needs monomix.
-		 */
-		snd_soc_update_bits(priv->codec, WM8962_THREED1,
-				WM8962_ADC_MONOMIX_MASK, WM8962_ADC_MONOMIX);
-	}
-
-	return 0;
-}
-
-static ssize_t show_headphone(struct device_driver *dev, char *buf)
-{
-	struct imx_priv *priv = &card_priv;
-	int hp_status;
-
-	if (!gpio_is_valid(priv->hp_gpio)) {
-		strcpy(buf, "no detect gpio connected\n");
-		return strlen(buf);
-	}
-
-	/* Check if headphone is plugged in */
-	hp_status = gpio_get_value(priv->hp_gpio) ? 1 : 0;
-
-	if (hp_status != priv->hp_active_low)
-		strcpy(buf, "headphone\n");
-	else
-		strcpy(buf, "speaker\n");
-
-	return strlen(buf);
-}
-
-static DRIVER_ATTR(headphone, S_IRUGO | S_IWUSR, show_headphone, NULL);
-
-static ssize_t show_mic(struct device_driver *dev, char *buf)
-{
-	struct imx_priv *priv = &card_priv;
-	int mic_status;
-
-	if (!gpio_is_valid(priv->mic_gpio)) {
-		strcpy(buf, "no detect gpio connected\n");
-		return strlen(buf);
-	}
-
-	/* Check if analog microphone is plugged in */
-	mic_status = gpio_get_value(priv->mic_gpio) ? 1 : 0;
-
-	if (mic_status != priv->mic_active_low)
-		strcpy(buf, "amic\n");
-	else
-		strcpy(buf, "dmic\n");
-
-	return strlen(buf);
-}
-
-static DRIVER_ATTR(microphone, S_IRUGO | S_IWUSR, show_mic, NULL);
-
-static int imx_tlv320aic3x_late_probe(struct snd_soc_card *card)
-{
-	struct snd_soc_dai *codec_dai = card->rtd[0].codec_dai;
-	struct imx_priv *priv = &card_priv;
-	struct imx_tlv320aic3x_data *data = snd_soc_card_get_drvdata(card);
-	struct device *dev = &priv->pdev->dev;
-	int ret;
-
-	ret = snd_soc_dai_set_sysclk(codec_dai, CLKIN_MCLK,
-			data->clk_frequency, SND_SOC_CLOCK_IN);
-	if (ret < 0)
-		dev_err(dev, "failed to set sysclk in %s\n", __func__);
-
-	return ret;
-}
-
-static int imx_tlv320aic3x_probe(struct platform_device *pdev)
+static int imx_sgtl5000_audmux_config(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
-	struct device_node *ssi_np, *codec_np;
-	struct platform_device *ssi_pdev;
-	struct imx_priv *priv = &card_priv;
-	struct i2c_client *codec_dev;
-	struct imx_tlv320aic3x_data *data;
-	struct clk *codec_clk = NULL;
 	int int_port, ext_port;
 	int ret;
-
-	priv->pdev = pdev;
 
 	ret = of_property_read_u32(np, "mux-int-port", &int_port);
 	if (ret) {
@@ -421,7 +95,7 @@ static int imx_tlv320aic3x_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "audmux internal port setup failed\n");
 		return ret;
 	}
-	imx_audmux_v2_configure_port(ext_port,
+	ret = imx_audmux_v2_configure_port(ext_port,
 			IMX_AUDMUX_V2_PTCR_SYN,
 			IMX_AUDMUX_V2_PDCR_RXDSEL(int_port));
 	if (ret) {
@@ -429,29 +103,43 @@ static int imx_tlv320aic3x_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ssi_np = of_parse_phandle(pdev->dev.of_node, "ssi-controller", 0);
+	return 0;
+}
+
+static int imx_sgtl5000_probe(struct platform_device *pdev)
+{
+	struct device_node *cpu_np, *codec_np;
+	struct platform_device *cpu_pdev;
+	struct i2c_client *codec_dev;
+	struct imx_sgtl5000_data *data;
+	int ret;
+
+	printk("*********** orenr probe 1");
+	cpu_np = of_parse_phandle(pdev->dev.of_node, "cpu-dai", 0);
 	codec_np = of_parse_phandle(pdev->dev.of_node, "audio-codec", 0);
-	if (!ssi_np || !codec_np) {
+	if (!cpu_np || !codec_np) {
 		dev_err(&pdev->dev, "phandle missing or invalid\n");
 		ret = -EINVAL;
 		goto fail;
 	}
 
-	ssi_pdev = of_find_device_by_node(ssi_np);
-	if (!ssi_pdev) {
+	if (strstr(cpu_np->name, "ssi")) {
+		ret = imx_sgtl5000_audmux_config(pdev);
+		if (ret)
+			goto fail;
+	}
+
+	cpu_pdev = of_find_device_by_node(cpu_np);
+	if (!cpu_pdev) {
 		dev_err(&pdev->dev, "failed to find SSI platform device\n");
 		ret = -EINVAL;
 		goto fail;
 	}
 	codec_dev = of_find_i2c_device_by_node(codec_np);
-	if (!codec_dev || !codec_dev->driver) {
+	if (!codec_dev) {
 		dev_err(&pdev->dev, "failed to find codec platform device\n");
-		ret = -EINVAL;
-		goto fail;
+		return -EINVAL;
 	}
-
-	priv->first_stream = NULL;
-	priv->second_stream = NULL;
 
 	data = devm_kzalloc(&pdev->dev, sizeof(*data), GFP_KERNEL);
 	if (!data) {
@@ -459,99 +147,72 @@ static int imx_tlv320aic3x_probe(struct platform_device *pdev)
 		goto fail;
 	}
 
-	codec_clk = devm_clk_get(&codec_dev->dev, NULL);
-	if (IS_ERR(codec_clk)) {
-		ret = PTR_ERR(codec_clk);
-		dev_err(&codec_dev->dev, "failed to get codec clk: %d\n", ret);
-		goto fail;
+	data->codec_clk = clk_get(&codec_dev->dev, NULL);
+	if (IS_ERR(data->codec_clk)) {
+		/* assuming clock enabled by default */
+		data->codec_clk = NULL;
+		ret = of_property_read_u32(codec_np, "clock-frequency",
+					&data->clk_frequency);
+		if (ret) {
+			dev_err(&codec_dev->dev,
+				"clock-frequency missing or invalid\n");
+			goto fail;
+		}
+	} else {
+		data->clk_frequency = clk_get_rate(data->codec_clk);
+		clk_prepare_enable(data->codec_clk);
 	}
-
-	data->clk_frequency = clk_get_rate(codec_clk);
-	clk_prepare_enable(codec_clk);
-
-	priv->amic_mono = of_property_read_bool(codec_np, "amic-mono");
-	priv->dmic_mono = of_property_read_bool(codec_np, "dmic-mono");
-
-	priv->hp_gpio = of_get_named_gpio_flags(np, "hp-det-gpios", 0,
-				(enum of_gpio_flags *)&priv->hp_active_low);
-	priv->mic_gpio = of_get_named_gpio_flags(np, "mic-det-gpios", 0,
-				(enum of_gpio_flags *)&priv->mic_active_low);
 
 	data->dai.name = "HiFi";
 	data->dai.stream_name = "HiFi";
 	data->dai.codec_dai_name = "tlv320aic3x-hifi";
-//	data->dai.codec_name	 = "tlv320aic3x",
-	data->dai.codec_of_node  = codec_np;
-//	data->dai.cpu_dai_name   = "imx-ssi.1";
-//	data->dai.platform_name	= "imx-pcm-audio.1",
-	data->dai.cpu_dai_name = dev_name(&ssi_pdev->dev);
-	data->dai.platform_of_node = ssi_np;
-	data->dai.ops = &imx_hifi_ops;
-	data->dai.init = &imx_tlv320aic3x_gpio_init;
-	data->dai.dai_fmt = SND_SOC_DAIFMT_DSP_B | SND_SOC_DAIFMT_CBM_CFM | SND_SOC_DAIFMT_IB_NF;
+	data->dai.codec_of_node = codec_np;
+	data->dai.cpu_of_node = cpu_np;
+	data->dai.platform_of_node = cpu_np;
+	data->dai.init = &imx_sgtl5000_dai_init;
+	data->dai.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+			    SND_SOC_DAIFMT_CBM_CFM;
 
 	data->card.dev = &pdev->dev;
 	ret = snd_soc_of_parse_card_name(&data->card, "model");
 	if (ret)
-		goto fail;
+		goto clk_fail;
 	ret = snd_soc_of_parse_audio_routing(&data->card, "audio-routing");
 	if (ret)
-		goto fail;
+		goto clk_fail;
 	data->card.num_links = 1;
+	data->card.owner = THIS_MODULE;
 	data->card.dai_link = &data->dai;
-	data->card.dapm_widgets = imx_tlv320aic3x_dapm_widgets;
-	data->card.num_dapm_widgets = ARRAY_SIZE(imx_tlv320aic3x_dapm_widgets);
-
-	data->card.late_probe = imx_tlv320aic3x_late_probe;
-
-	platform_set_drvdata(pdev, &data->card);
-	snd_soc_card_set_drvdata(&data->card, data);
+	data->card.dapm_widgets = imx_sgtl5000_dapm_widgets;
+	data->card.num_dapm_widgets = ARRAY_SIZE(imx_sgtl5000_dapm_widgets);
 
 	ret = snd_soc_register_card(&data->card);
 	if (ret) {
 		dev_err(&pdev->dev, "snd_soc_register_card failed (%d)\n", ret);
-		goto fail;
+		goto clk_fail;
 	}
 
-	if (gpio_is_valid(priv->hp_gpio)) {
-		ret = driver_create_file(pdev->dev.driver, &driver_attr_headphone);
-		if (ret) {
-			dev_err(&pdev->dev, "create hp attr failed (%d)\n", ret);
-			goto fail_hp;
-		}
-	}
-
-	if (gpio_is_valid(priv->mic_gpio)) {
-		ret = driver_create_file(pdev->dev.driver, &driver_attr_microphone);
-		if (ret) {
-			dev_err(&pdev->dev, "create mic attr failed (%d)\n", ret);
-			goto fail_mic;
-		}
-	}
-
-	goto fail;
-
-fail_mic:
-	driver_remove_file(pdev->dev.driver, &driver_attr_headphone);
-fail_hp:
-	snd_soc_unregister_card(&data->card);
+	platform_set_drvdata(pdev, data);
+clk_fail:
+	clk_put(data->codec_clk);
 fail:
-	if (ssi_np)
-		of_node_put(ssi_np);
+	if (cpu_np)
+		of_node_put(cpu_np);
 	if (codec_np)
 		of_node_put(codec_np);
 
 	return ret;
 }
 
-static int imx_tlv320aic3x_remove(struct platform_device *pdev)
+static int imx_sgtl5000_remove(struct platform_device *pdev)
 {
-	struct snd_soc_card *card = platform_get_drvdata(pdev);
+	struct imx_sgtl5000_data *data = platform_get_drvdata(pdev);
 
-	driver_remove_file(pdev->dev.driver, &driver_attr_microphone);
-	driver_remove_file(pdev->dev.driver, &driver_attr_headphone);
-
-	snd_soc_unregister_card(card);
+	if (data->codec_clk) {
+		clk_disable_unprepare(data->codec_clk);
+		clk_put(data->codec_clk);
+	}
+	snd_soc_unregister_card(&data->card);
 
 	return 0;
 }
@@ -566,15 +227,14 @@ static struct platform_driver imx_tlv320aic3x_driver = {
 	.driver = {
 		.name = "imx-tlv320aic3x",
 		.owner = THIS_MODULE,
-		.pm = &snd_soc_pm_ops,
 		.of_match_table = imx_tlv320aic3x_dt_ids,
 	},
-	.probe = imx_tlv320aic3x_probe,
-	.remove = imx_tlv320aic3x_remove,
+	.probe = imx_sgtl5000_probe,
+	.remove = imx_sgtl5000_remove,
 };
 module_platform_driver(imx_tlv320aic3x_driver);
 
 MODULE_AUTHOR("ron.d@variscite.com. Variscite Ltd.");
 MODULE_DESCRIPTION("Variscite i.MX TLV320aic3x ASoC machine driver");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:imx-wtlv320aic3x");
+MODULE_ALIAS("platform:imx-tlv320aic3x");
